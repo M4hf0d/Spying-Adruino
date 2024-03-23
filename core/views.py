9782fb931312b84923616ed775e1ea34f36ca496
Sayah Mahfoud Abdel Ali <73748467+M4hf0d@users.noreply.github.com>
2024-03-24 00:38:39 +0100

    static file

--- core/views.py
+++ core/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import GPSData,Data
 from .serializers import GPSDataSerializer,DataSerializer
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 class GPSDataListCreateView(generics.ListCreateAPIView):
     queryset = GPSData.objects.all()
     serializer_class = GPSDataSerializer
 
+@csrf_exempt
 class DataListCreateView(generics.ListCreateAPIView):
     queryset = Data.objects.all()
     serializer_class = DataSerializer
